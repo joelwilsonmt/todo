8ea6fa7ba5811d840ecdeee611d9da3cd57302e7
Zack Viente <Zack@Zacks-MacBook-Air.local>
2018-10-03 16:04:53 -0600

    Updated JS added feature to add item and contiune input

--- js/queryscript.js
+++ js/queryscript.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
 
+// Listen for click on button and adds li to ul
 
+      $('.addBtn').on('click', function(e){
+        e.preventDefault();
+      });
+      $('p').keydown(function(e){
+        if (e.which == 13){
+          e.preventDefault();
+          $(this).blur();
+        }
+      });
+
+      $('.addBtn').on('click', function(){
+        $('ul').append("<li class='listitem'>"+
+          '<a href="" class="donebtn" onmousedown="return false">Done</a>'+
+          '<div class="right">'+
+            '<p contenteditable="true">Go to work</p>'+
+          "</div>"+
+          '<a href="" class="deleteBtn" onmousedown="return false">X</a>'+
+        "</li>").find('li:last-child');
+
+        $('li:last-child p').text('New Task...');
+        $('li:last-child p').on('click', function() {
+          $(this).empty();
+        });
+        $('li:last-child p').keydown(function(e){
+          if (e.which == 13){
+            e.preventDefault();
+            $(this).blur();
+          }
+        });
+      });
 
 });//End ready function
