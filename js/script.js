eeccd7cd3fdca6cbfc5136fb336f32888ad47bfa
Zack Viente <Zack@Zacks-MacBook-Air.local>
2018-10-04 12:58:03 -0600

    changed if statement to switch

--- js/script.js
+++ js/script.js
@@ -4,14 +4,29 @@
   var hourNow = today.getHours();
   var greeting;
 
-    if (hourNow > 18){
-      greeting = 'Good Evening';
-    }  else if (hourNow > 12){
-      greeting = 'Good Afternoon';
-    } else if (hourNow > 0){
-      greeting = 'Good Morning';
-    } else {
-      greeting = 'Welcome';
+    // if (hourNow > 18){
+    //   greeting = 'Good Evening';
+    // }  else if (hourNow > 12){
+    //   greeting = 'Good Afternoon';
+    // } else if (hourNow > 0){
+    //   greeting = 'Good Morning';
+    // } else {
+    //   greeting = 'Welcome';
+    // }
+
+    switch (true) {
+      case hourNow > 18:
+        greeting = 'Good Evening';
+          break;
+      case hourNow > 12:
+        greeting = 'Good Afternoon';
+          break;
+      case hourNow > 0:
+        greeting = 'Good Morning';
+          break;
+      default:
+        greeting = 'Welcome';
     }
+    
 
   $('#timegreet').text(greeting);
